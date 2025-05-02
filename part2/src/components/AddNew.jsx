4005bb59afaeb05a3d95b724aa03ca163cb46f18
David <EstebanDav23@gmail.com>
2025-05-02 12:25:29 -0500

    addNew corrected

--- part2/src/components/AddNew.jsx
+++ part2/src/components/AddNew.jsx
@@ -3,64 +3,64 @@ import axios from 'axios'
 import noteService from "../services/notes";
 
 const AddNew = ({persons, setPersons, contextNotification}) => {
-    const [newName, setNewName] = useState('')
-    const [newNumber, setNewNumber] = useState('')
-  
-    const addName = (event) => {
-        event.preventDefault()
-        console.log('button clicked', event.target)
-        const nameObject = {
-          name: newName,  
-          number: newNumber, 
-         }
-         persons.map(person => person.name)
-        
-         if (persons.map(person => person.name).includes(newName)) {
-          window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)
-          const person = persons.find(p => p.name === newName);
-          axios
-          .put(`http://localhost:3001/persons/${person.id}`, nameObject)
-          .then((response) => {
-          setPersons(persons.map(p => p.id !== person.id ? p : response.data))
-          contextNotification("success", `Updated ${newName}`)
-          return response.data
-          })
-        }
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-          else {
-          
-        noteService
+  const addName = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    const nameObject = {
+      name: newName,  
+      number: newNumber, 
+    }
+
+    const existingPerson = Array.isArray(persons)
+      ? persons.find(person => person.name === newName)
+      : null;
+
+    if (existingPerson) {
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+        axios
+          .put(`http://localhost:3001/persons/${existingPerson.id}`, nameObject)
+          .then((response) => {
+            setPersons(persons.map(p => p.id !== existingPerson.id ? p : response.data))
+            contextNotification("success", `Updated ${newName}`)
+          })
+          .catch(() => {
+            contextNotification("error", `Error updating ${newName}`)
+          });
+      }
+    } else {
+      noteService
         .create(nameObject)
         .then(response => {
-        setPersons(persons.concat(response.data))
+          setPersons(persons.concat(response.data))
+          contextNotification("success", `Added ${newName}`)
+        })
+        .catch(() => {
+          contextNotification("error", `Error adding ${newName}`)
+        });
+    }
+  }
 
-        contextNotification("success", `Added ${newName}`)
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
 
-      })}
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
 
-
-      }
-    
-      const handleNameChange = (event) => {
-        console.log(event.target.value)
-        setNewName(event.target.value)
-      }
-    
-      const handleNumberChange = (event) => {
-        console.log(event.target.value)
-        setNewNumber(event.target.value)
-      }
-  
-    return (
+  return (
     <div>
       <h2>Add New</h2>
       <form onSubmit={addName}>
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          number: <input value={newNumber} onChange={handleNumberChange}/>
-          </div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
         <div>
           <button type="submit">add</button>
         </div>
